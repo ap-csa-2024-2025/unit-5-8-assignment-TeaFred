17f93096a06dcaa2ea3b6b5bcbe79c25ec642f7c
TeaFred <fhtruong100@student.hbuhsd.edu>
2025-01-08 19:04:14 +0000

    fghfgh

--- Dog.java
+++ Dog.java
@@ -2,15 +2,48 @@ public class Dog
 {
   private String name;
   private int ID;
-  private static int numDogs = 0;
-
-  // Non-default Dog constructor
-
+  private static int numDogs = 0; //Static field, meaning its accessed by all and it isn't limited to just the one dog. Every time you make a dog it updates the larger program.
+  //By making numDogs static, it belongs to the class so you can easily keep track of it, numDogs isn't a property limited to just one object
+  //If you make numDogs not static, each dog gets its own numDogs, it is counted by itself, which is bad.
+  //Non Default Constructor
+  public Dog(String name)
+  {
+    this.name = name;
+    numDogs ++;
+    this.ID = numDogs * numDogs; 
+  }
   // Default Dog constructor
 
   // Setters - instance variables only
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+
+  public void setID (int ID)
+  {
+    this.ID = ID;
+  }
+
+  public void setnumDogs (int numDogs)
+  {
+    this.numDogs = numDogs;
+  }
 
   // Getters - instance variables only
 
-  // method getNumDogs
+  public String getName()
+  {
+    return name;
+  }
+
+  public int getID()
+  {
+    return ID;
+  }
+
+  public int getnumDogs()
+  {
+    return numDogs;
+  }
 }
--- Main.java
+++ Main.java
@@ -4,6 +4,11 @@ public class Main
 {
   public static void main(String[] args)
   {
+    Dog dog1 = new Dog();
+    Dog dog2 = new Dog();
+    dog1.setName("Clam");
+    dog2.setID(0927);
+
     Scanner sc = new Scanner(System.in);
     String input = "";
     while (!input.equals("q"))
